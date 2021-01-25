979c3032da920c218bdf2b23e36d1ecebb198911
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-01-25 14:16:01 -0600

    Added extra comments

--- Project2/DigitCounter.cpp
+++ Project2/DigitCounter.cpp
@@ -1,3 +1,9 @@
+/*
+	John Knudson
+	CSCI 301 - Julstrom
+	Project 2 - File 1 - Number of digits in an integer
+*/
+
 //This program uses a recursive function to determine the number of digits in a given positive integer
 
 
@@ -11,23 +17,23 @@ int IntegerDigits(int userInt);
 int main()
 {
 	//Get integer from user
-	cout << "Enter a positive integer and I will tell you how many digits it has\n";	//prompt user for integer  input
-	int userInteger;																		//Variable to hold user integer 
-	cin >> userInteger; 
+	cout << "Enter a positive integer and I will tell you how many digits it has\n";	//prompt user for integer input
+	int userInteger;																	//Variable to hold user integer 
+	cin >> userInteger;																	//Get integer to count digits from user 	
 
-	int digits = IntegerDigits(userInteger);	//call 
-	cout << "This integer contains " << digits << " digits.";
+	int digits = IntegerDigits(userInteger);											//call IntegerDigits to calculate number of digits in user's integer
+	cout << "This integer contains " << digits << " digits.";							//Print number of digits to terminal 
 	return 0;
 }
 
 int IntegerDigits(int userInt)
 {
 	static int intDigits = 0;				//variable to hold value for number of digits in the Integer
-	if (userInt < 10) {					//ASSERT: userInt is a single digit
-		intDigits += 1;					//Increment our digit count by 1
-		return intDigits;				//Return intDigits which holds total of digits in userInt
+	if (userInt < 10) {						//ASSERT: userInt is a single digit
+		intDigits += 1;						//Increment our digit count by 1
+		return intDigits;					//Return intDigits which holds total of digits in userInt
 	}
-	else {								//ASSERT: userInt > 10 and contains more than one digit 
+	else {									//ASSERT: userInt > 10 and contains more than one digit 
 		intDigits++;
 		return IntegerDigits(userInt/10);
 	}
