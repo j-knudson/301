83e3dbb2f9f36bc5f1e073a5f3962d57ad2f91fa
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-01-27 12:21:45 -0600

    Update ReverseString.cpp

--- Project2/ReverseString.cpp
+++ Project2/ReverseString.cpp
@@ -5,34 +5,34 @@ using namespace std;
 
 
 void reverse(char s[], int n);										//function returns an array of character in reverse 
-//Precondition: S is a char array with a size of N 
+//Precondition: s is a char array with a size of n; n is a non-negative integer 
 //Postcondition: Characters of S were printed in reverse order 
 
 int main()
 {
 	
 	cout << "Please enter a string of characters and they will be returned in reverse order\n"; //prompt user for a string
-	string userString;																			//variable for users input
-	cin >> userString;																			//get string from user
-	int stringLength = userString.length();														//calculate length of the user's input string	
+	string userString;														//variable for users input
+	cin >> userString;														//get string from user
+	int stringLength = userString.length();												//calculate length of the user's input string	
 	
-	char* charArray = new char[stringLength+1];													//create a character array based on size of string (length+1 for null terminator)
+	char* charArray = new char[stringLength+1];											//create a character array based on size of string (length+1 for null terminator)
 	strcpy_s(charArray, stringLength+1, userString.c_str());									//copy contents of userString into our new array
 	
-	cout << "The reversed string is: ";															//report to user on terminal
-	reverse(charArray, stringLength);															//call reverse function to print the array in reverse	
+	cout << "The reversed string is: ";												//report to user on terminal
+	reverse(charArray, stringLength);												//call reverse function to print the array in reverse	
 
 	return 0;
 }
 
 void reverse(char s[], int n)
 {
-	if (n == 0)						//base case, an array of a single element is printed
+	if (n == 0)					//base case, an array of a single element is printed
 		cout << s[n];
-	else                            //ASSERT: n > 0   
+	else                            		//ASSERT: n > 0   
 	{
 		cout << s[n];				//print last element in charArray[s]
-		return reverse(s, n - 1);	//call reveerse function and pass array with one less element 
+		return reverse(s, n - 1);		//call reveerse function and pass array with one less element 
 	}
 }
 	
