503541f3848154115bbf131a0604fb50c5249d7f
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-02-13 16:44:55 -0600

    Starting point

---
+++ Test1/recStar.cpp
@@ -0,0 +1,28 @@
+/*
+	Recursion Practice
+  Generate a triangle of n *'s recursively
+*/
+
+/*
+	What is a smaller solution?
+		Generate n-1 stars
+	How to use smaller solution?
+		Call to Print (n-1) stars
+	What is the base case?
+		if n == 1, print 1 star if n == 0 don't print
+*/
+
+#include <iostream>
+void pattern(int NumStars);
+
+int main()
+{
+
+  return 0;
+}
+
+void pattern(int NumStars)
+{
+
+  
+}
