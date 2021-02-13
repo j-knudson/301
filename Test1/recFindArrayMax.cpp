bf713634a5d11a7932896191311c1f0bf131cab3
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-02-13 16:12:44 -0600

    Starting Point

---
+++ Test1/recFindArrayMax.cpp
@@ -0,0 +1,27 @@
+/*
+	Recursion Practice
+  Find the largest value in an array from index low to index high
+*/
+
+/*
+	What is a smaller solution?
+		From low+1 to high
+	How to use smaller solution?
+		compare low+1 to high to low
+	What is the base case?
+		if only one element low = high
+*/
+
+#include <iostream>
+int max(int A[], int low, int high);
+
+int main()
+{
+
+  return 0;
+}
+
+int max(int A[], int low, int high)
+{
+  
+}
