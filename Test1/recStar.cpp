5418d76a5613023c224ccfd2936cd3eed0e79219
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-02-13 17:00:22 -0600

    my initial try

--- Test1/recStar.cpp
+++ Test1/recStar.cpp
@@ -14,15 +14,33 @@
 
 #include <iostream>
 void pattern(int NumStars);
+void asterisks(int k);
 
 int main()
 {
-
+  std::cout << "Enter an integer and I will make a triangle of *'s with a max line of that number" << '\n';
+  int stars;
+  std::cin >> stars;
+  pattern(stars);
   return 0;
 }
 
 void pattern(int NumStars)
 {
+  if (NumStars == 1)
+    std::cout << '*' << '\n';
+  else
+    pattern(NumStars-1);
+    asterisks(NumStars);
+    std::cout << '\n';
 
-  
+}
+
+void asterisks(int k)
+{
+  if (k > 0)
+  {
+    asterisks(k-1);
+    std::cout << "*";
+  }
 }
