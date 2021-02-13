4f8eee4869f43e5e76cfc808e16ab10d472886dc
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-02-13 15:59:55 -0600

    Refactored function

--- Test1/SumOfN.cpp
+++ Test1/SumOfN.cpp
@@ -9,6 +9,31 @@
 	How to use smaller solution?
 		if n > 1  return sum+= sum(n-1)
 	What is the base case?
-		if n <= 1 sum = n 
+		if n <= 1 sum = n
 */
 #include <iostream>
+int SumOfN(int num);
+
+int main()
+{
+  std::cout << "Enter an integer and I will print the sum of each number up to that integer \n";
+  int userNum;
+  std::cin >> userNum;
+  int sum = SumOfN(userNum);
+  std::cout << "The sum of first " <<userNum <<" integers is " <<sum <<std::endl;
+  return 0;
+}
+
+int SumOfN(int num)
+{
+  //static int total;
+  if (num < 1)
+    return num;
+  else
+  {
+    //total += num;
+    return SumOfN(num-1)+num;
+  }
+  //return total;
+
+}
