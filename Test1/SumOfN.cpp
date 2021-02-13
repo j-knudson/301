884ce3d9597f6f2738e87b000836331bd279ed56
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-02-13 15:24:42 -0600

    starting point

---
+++ Test1/SumOfN.cpp
@@ -0,0 +1,14 @@
+/*
+	Recursion Practice
+	Return the sum of first N integers
+*/
+
+/*
+	What is a smaller solution?
+		what is num of n-1?
+	How to use smaller solution?
+		if n > 1  return sum+= sum(n-1)
+	What is the base case?
+		if n <= 1 sum = n 
+*/
+#include <iostream>
