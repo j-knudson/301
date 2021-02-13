aface2a1599bc4265f51bd365ca12e852c9cc6f7
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-02-13 17:42:51 -0600

    starting point

---
+++ Test1/recBinarySearch.cpp
@@ -0,0 +1,23 @@
+/*
+	Recursion Practice
+  Search a sorted array for a specific value
+*/
+
+/*
+	What is a smaller solution?
+		if n > 1; search n/2
+	How to use smaller solution?
+	  split an array into two parts and elimante one part that is too large/small
+	What is the base case?
+		if array has one element, is A[n] our target value?
+*/
+
+bool binSearch(int A[], int target);
+
+int main()
+{
+
+  return 0;
+}
+
+bool binSearch(int A[], int target)
