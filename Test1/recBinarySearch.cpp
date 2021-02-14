78ebe049278343e51ad19796c412aabf73153a1e
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-02-13 18:13:50 -0600

    Updated binSearch()   corrected updating low/high/mid on recursive call  had low/high switch with mid backwards

--- Test1/recBinarySearch.cpp
+++ Test1/recBinarySearch.cpp
@@ -11,13 +11,37 @@
 	What is the base case?
 		if array has one element, is A[n] our target value?
 */
+#include <iostream>
 
-bool binSearch(int A[], int target);
+bool binSearch(int A[], int target, int low, int high);
 
 int main()
 {
+  std::cout << "Enter an integer and I will report if that value is in the array" << '\n';
+  int userNum;
+  std::cin >> userNum;
 
+  int B[10] = {1,2,3,4,5,6,7,8,9,10};
+
+  bool found = binSearch(B, userNum, 0, 9);
+  if (found)
+  {
+    std::cout << "The value " << userNum << " was found" << '\n';
+  }
+  else
+    std::cout << "The value " << userNum << " was NOT found" << '\n';
   return 0;
 }
 
-bool binSearch(int A[], int target)
+bool binSearch(int A[], int target, int low, int high)
+{
+  int mid = (low + high)/2;
+  if (low > high)
+    return false;
+  else if (A[mid] < target)
+    return binSearch(A, target, mid+1, high);
+  else if (A[mid] == target)
+    return true;
+  else
+    return binSearch(A, target, low, mid-1);
+}
