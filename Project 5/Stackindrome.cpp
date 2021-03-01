feb09ba146fee0c79cd70cc14b8bb728a1e319dc
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-03-01 16:18:55 -0600

    Update Stackindrome.cpp

--- Project 5/Stackindrome.cpp
+++ Project 5/Stackindrome.cpp
@@ -9,17 +9,17 @@
 
 int main()
 {
-	std::cout << "Please enter a string with no more than 100 characters, and I will let you know if the string is a palindrome\n"; // prompt user for a string
+	std::cout << "Please enter a string with no more than 42 characters, and I will let you know if the letters of the string are a palindrome\n"; // prompt user for a string
 
 	char ch; //variable to hold one letter of user string 
 	Stack InputStack; //create first stack which holds all the values from the user string  
 	std::cin.get(ch);	//read inputs one character at a time	
 
-	while (ch >= ' ')		//as long as there is a character to read; checks ch is at least as largest as smallest ASCII value   
+	while (ch >= ' ')		//as long as there is a character to read  
 	{
-		if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))  	//make sure user character falls within alpha character range  
-			InputStack.push(ch);					//add all letters to the Input stack
-		std::cin.get(ch);						//get the next character from the user string 
+		if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))  //make sure user character falls within alpha character range  
+			InputStack.push(ch);									//add all letters to the Input stack
+		std::cin.get(ch);										//get the next character from the user string 
 	}
 
 	size_t userStringSize = InputStack.size(); //Calculate the length of the users string
@@ -27,8 +27,8 @@ int main()
 	
 	while (PalindromeCheck.size() < userStringSize/2)  // Remove the second half of the characters from the first stack and put them into the second stack working down to the midpoint
 	{
-		ch = InputStack.pop();				//removes the top character from the first stack
-		PalindromeCheck.push(ch);			//adds that character to the second stack starting at the bottom and then adding on top of it 
+		ch = InputStack.pop();						//removes the top character from the first stack
+		PalindromeCheck.push(ch);					//adds that character to the second stack starting at the bottom and then adding on top of it 
 	}
 
 	if (!InputStack.empty())			// in case the stack had an odd number of characters and there is one character left 
@@ -37,13 +37,13 @@ int main()
 	bool isPalindrome = true;			//create a bool flag used to see if the two stacks match
 	while (!InputStack.empty() && !PalindromeCheck.empty() && isPalindrome)	//Compare the characters in the two stacks until they are empty or a mismatch is found 
 	{
-		ch = InputStack.pop();						//get the top character from the initial stack 
-		char ChComparison = PalindromeCheck.pop();			//get the top character from the second stack 
-		if (ch != ChComparison)						//compare them to see if they match
+		ch = InputStack.pop();								//get the top character from the initial stack 
+		char chComparison = PalindromeCheck.pop();			//get the top character from the second stack 
+		if (ch != chComparison)								//compare them to see if they match
 			isPalindrome = false; 
 	}
 
-	if (isPalindrome)							//report to terminal if the user's string was a palindrome 
+	if (isPalindrome)										//report to terminal if the user's string was a palindrome 
 		std::cout << "The string IS a palindrome\n"; 
 	else
 		std::cout << "The string IS NOT a palindrome \n";
