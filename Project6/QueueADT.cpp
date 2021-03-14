f424a5ab099817c08a4513aba7e1f13e8e3133cd
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-03-14 18:14:54 -0500

    Added overloaded << function and a dequeue function

--- Project6/QueueADT.cpp
+++ Project6/QueueADT.cpp
@@ -21,6 +21,8 @@ Queue::Queue()				//creates an empty Queue
 	count = 0;				//number of items in the queue
 }
 
+//////////////
+//Modification Member Functions 
 void Queue::enqueue(const Item& entry)
 {
 	if (count < CAPACITY)		//check if there is room in the queue
@@ -31,3 +33,28 @@ void Queue::enqueue(const Item& entry)
 	}
 }
 
+Queue::Item Queue::dequeue()
+{
+	if (count > 0)			//check if there is an item in the Queue
+	{
+		Item deqValue = data[front];	//get the value from the front of the queue
+		front = NextIndex(front);		//advance front to next element in array
+		--count;						//decrement count of items in queue
+		return deqValue;				//return retrieved value 
+	}
+}
+
+////////////
+//Friend Function 
+
+std::ostream& operator << (std::ostream& out_s, Queue q)
+{
+	std::size_t i, index; 
+	index = q.front;		//set index to the front of the queue
+	for (i = 0; i < q.count; ++i)		//count holds number of items in the queue; start at front and advance until quantity of count elements visited
+	{
+		out_s << q.data[index] << ' ';	//print the value of the element and a space
+		index = q.NextIndex(index);		//advance to the next element 
+	}
+	return out_s; //return the ostream
+}
