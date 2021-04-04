b666020a792725ee8cc52e0b45597d8c9950f1aa
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-04-04 09:53:47 -0500

    Updated comments in code

--- Project9/ExpressionTree.cpp
+++ Project9/ExpressionTree.cpp
@@ -27,6 +27,8 @@ void ExpTree::build(std::istream& infile)
 	root = helpBuild(infile); //now call the help build function to recursively build tree from root; infile is our input stream 
 }
 
+/// ///
+/// Public functions that call recursive pair
 void ExpTree::prefix(std::ostream& outfile)	//writes a prefix expression
 {
 	return printPrefix(outfile, root);
@@ -43,37 +45,41 @@ int ExpTree::value() //returns value of the expression
 {
 	return findValue(root);
 }
+
+
+/// ///
+/// Private Recursive functions 
 void ExpTree::destroy(Node* root) //Used to give back nodes
 {
-	if (root != NULL) //if there are children that need to be deleted
-	{	
+	if (root != NULL)			//Check if the root has a value 
+	{							//first check if there are children that need to be deleted
 		destroy(root->left);	//delete left branch
 		destroy(root->right);	//delete right branch
-		delete root; 
+		delete root;			//Last step: Destroy the root 
 	}
 }
 ExpTree::Node* ExpTree::helpBuild(std::istream& infile)
 {
 	char ch;
-	Node* temp;
+	Node* temp;			
 
 	infile >> ch; //read character
 	if (isdigit(ch))		//base case --> character read is a digit
 	{
-		temp = new Node;
+		temp = new Node;	//create a new node to hold the value
 		temp->operand = ch - '0';  //using subtract ascii value 0 trick to store ch as an integer value 
-		temp->left = NULL;
+		temp->left = NULL;	
 		temp->right = NULL;
 	}
 	else // ch is '('	--> start of a subexpression only has choices betwee ( or a digit 
 	{
 		temp = new Node;
-		temp->left = helpBuild(infile);
-		infile >> temp->optr;
-		temp->right = helpBuild(infile);
-		infile >> ch; // ')' 
+		temp->left = helpBuild(infile); //left paren was consumed now make recursive call to get digit or subexpression to left 
+		infile >> temp->optr;			//read the operator and store it 
+		temp->right = helpBuild(infile);	//recursive call to store the subexpression (or digit) to the right of operator 
+		infile >> ch; // ')'			//last step to consume the right paren 
 	}
-	return temp;
+	return temp;						//return the node to place it on the tree
 }
 void ExpTree::printPrefix(std::ostream& outfile, Node* r)	//preorder traversal 
 {
@@ -86,7 +92,7 @@ void ExpTree::printPrefix(std::ostream& outfile, Node* r)	//preorder traversal
 		printPrefix(outfile, r->right);
 	}
 }
-void ExpTree::printInfix(std::ostream& outfile, Node* r)
+void ExpTree::printInfix(std::ostream& outfile, Node* r) //print expression in Infix notation 
 {
 	if (r->left == NULL)  //leaf?  if so print operand
 		outfile << r->operand << ' ';
@@ -99,7 +105,7 @@ void ExpTree::printInfix(std::ostream& outfile, Node* r)
 		outfile << ") "; //fully parenethisized infix, so add right paren
 	}
 }
-void ExpTree::printPostfix(std::ostream& outfile, Node* r)
+void ExpTree::printPostfix(std::ostream& outfile, Node* r)  //print expression in postfix notation 
 {
 	if (r->left == NULL)  //leaf?  if so print operand
 		outfile << r->operand << ' ';
@@ -110,19 +116,19 @@ void ExpTree::printPostfix(std::ostream& outfile, Node* r)
 		outfile << r->optr << ' ';	//postfix   end with printing operator
 	}
 }
-int ExpTree::findValue(Node* r)
+int ExpTree::findValue(Node* r) //function to return the result of the expression 
 {
 	if (r->left == NULL)  // Leaf--> operand
 		return r->operand;
 	else  //interior node -->operator
 	{
-		if (r->optr == '+')
+		if (r->optr == '+')  //if the operator is  + add the left and right branches
 			return findValue(r->left) + findValue(r->right);
-		else if (r->optr == '-')
+		else if (r->optr == '-')	//if the operators is - subtract the right branch from the lef
 			return findValue(r->left) - findValue(r->right);
-		else if (r->optr == '*')
+		else if (r->optr == '*')	//if hte operator is * multiply both sides
 			return findValue(r->left) * findValue(r->right);
-		else //(r->optr == '/')
+		else //(r->optr == '/')   divide the left side by the right side 
 			return findValue(r->left) / findValue(r->right);
 	}
 }
--- Project9/ExpressionTree.h
+++ Project9/ExpressionTree.h
@@ -13,20 +13,30 @@
 class ExpTree
 {
 public:
-	//Copy constructor
+	//////
+	//Default constructor
 	ExpTree() { root = NULL; }	
 	//Postcondition: Initializes an empty tree
 	
+	//////
+	//Default Destructor 
 	~ExpTree(); 
 	//Postcondition: Returns nodes
 	
 	void build(std::istream& infile);
+	//Preconditiion: Input stream is open and available for reading
+	//Postcondition: An expression tree was built from the input file's expression 
 
 	
 	void prefix(std::ostream& outfile);	//writes a prefix expression
+	//Postcondition: The prefix form of the expression was printed to the output stream
 	void infix(std::ostream& outfile);	//writes an infix expression
+	//Postcondition: The infix form of the expression was printed to the output stream
 	void postfix(std::ostream& outfile);	//writes a postfix expression 
+	//Postcondition: The postfix form of the expression was printed to the output stream
 	int value(); //returns value of the expression 
+	//Precondition: The expression has been built and stored in a tree 
+	//Postcondition: Returns the resulting value from completing the expression computation 
 
 
 private:
@@ -35,16 +45,24 @@ private:
 		int operand;		//holds the operand 
 		char optr;			//holds an operator
 		Node* left;			//for the left branch --> Part of expression to the left of operator
-		Node* right;		//for the right branch	--> Part of expressino to right of operator 
+		Node* right;		//for the right branch	--> Part of expression to right of operator 
 	};
 	Node* root; 
 
 	void destroy(Node* root); //Used to give back nodes
+	//Postcondition: All nodes in the tree have been deleted and returned 
 	Node* helpBuild(std::istream& infile);
+	//Precondition: An input stream is open from which values can be read 
+	//Postcondition: A tree has been built from the expressions in the input stream 
 	void printPrefix(std::ostream& outfile, Node* r);
+	//Postcondition: The prefix form of the expression was printed to the output stream
 	void printInfix(std::ostream& outfile, Node* r);
+	//Postcondition: The infix form of the expression was printed to the output stream
 	void printPostfix(std::ostream& outfile, Node* r);
+	//Postcondition: The postfix form of the expression was printed to the output stream
 	int findValue(Node* r);
+	//Precondition: To find the value of the entire expression r needs to start as the root of the tree
+	//Postcondition: Returns the resulting value from completing the expression computation 
 };
 
 #endif; 
